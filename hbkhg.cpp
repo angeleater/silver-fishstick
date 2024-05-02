5c6cb89ccea1060d25eaf86c3265f6566f7b8107
angeleater <165410040+angeleater@users.noreply.github.com>
2024-05-02 15:41:07 -0400

    Add files via upload

---
+++ hbkhg.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int daysLeft = 5;
+str userCommand = "none";
+str userObject = "none";
+bool userInputValid = ;
+
+//game start
+
+int main ()
+{
+    cout << "It is dark, and you are alone. Very alone.\n";
+    cout << "What can you do? \n";
+    cout << "(Type commands like 'use', 'see', or 'hit')\n";
+
+    cin >> userCommand;
+
+if (userInputValid = true) {
+
+    cout << "You " << userCommand <<" "<< userObject << ".";
+}
+
+else 
+    cout << "I'm sorry, I don't understand.";
+
+
+return 0;
+}
+
+    cout << "You have" << daysLeft << "days of oxygen left.";
